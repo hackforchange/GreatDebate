9182633a42cdeae5400bad357f5066bf0a796e98
Daniel Mican <dm03514@gmail.com>
2011-06-18 19:08:46 +0000

    urls now serve medial locally

--- greatdebate/urls.py
+++ greatdebate/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
@@ -15,3 +16,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.SERVE_MEDIA:
+  urlpatterns += patterns("", url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True,}),)
