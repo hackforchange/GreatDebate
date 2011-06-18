19feb4a3ba4e9f1824f125db9c58fbf1dad4d9bd
Daniel Mican <dm03514@gmail.com>
2011-06-18 23:21:13 +0000

    tests we can save a dm with save_dm

--- greatdebate/apps/campaigns/tests.py
+++ greatdebate/apps/campaigns/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.test.client import Client
 from greatdebate.apps.campaigns.models import Campaign
+from greatdebate.apps.decisionMakers.models import DecisionMaker
 
 class CampaignsTestCase(TestCase):
 
@@ -10,7 +11,7 @@ class CampaignsTestCase(TestCase):
 
   def test_save_campaign_success_no_dm(self):
     """
-    Tests we can sucessfully save a campaign with no user.
+    Tests we can sucessfully save a campaign with no dm.
     """
     post_params = {
       'campaign_url': 'uniquetestcampaign.com',
@@ -21,3 +22,20 @@ class CampaignsTestCase(TestCase):
     new_campaign_count = Campaign.objects.filter(campaign_url=post_params['campaign_url']).count()
     self.assertEqual(new_campaign_count, 1)
     #import ipdb; ipdb.set_trace()
+
+
+  def test_save_campaign_success_with_dm(self):
+    """
+    Tests we can sucessfully save a campaign with a dm.
+    """
+    post_params = {
+      'campaign_url': 'uniquetestcampaign.com',
+      'decision_makers': '1',
+      'email': 'test@test.com',
+    }
+    new_dm = DecisionMaker(name='Barak Obama', title='Commander and Chef')
+    new_dm.save()
+    response = self.client.post('/save_campaign/', post_params)
+    new_campaign = Campaign.objects.filter(campaign_url=post_params['campaign_url'])
+    self.assertEqual(new_campaign.count(), 1)
+    self.assertEqual(new_campaign[0].decision_maker.all()[0], new_dm)
