246f543ef63fc5c616f741a321dd710b955de46c
Paul Capriolo <pcapriolo@pelican.dyn-o-saur.com>
2011-06-18 20:35:25 +0000

    created activist model

--- greatdebate/urls.py
+++ greatdebate/urls.py
@@ -1,8 +1,6 @@
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
-"""
-test
-"""
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
