66e65e058bfd22db72faa75a20a2313fdb397b87
Daniel Mican <dm03514@gmail.com>
2011-06-19 02:40:29 +0000

    Revert "Can server static media."
    This reverts commit 83aa7772b00961c28e97502c33755b21cc2e63fe.

--- greatdebate/apps/organizers/views.py
+++ greatdebate/apps/organizers/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render_to_response
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import requires_POST
 from greatdebate.apps.organizers.models import Organizer
 
 def new_organizer(request):
@@ -12,7 +11,3 @@ def new_organizer(request):
   organizer = Organizer(**organizer_insert_dict)
   organizer.save()
   return HttpResponseRedirect('/create_campaign')
-
-def home(request):
-  """Server home template"""
-  return render_to_response('home.html')
--- greatdebate/settings.py
+++ greatdebate/settings.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 DEBUG = False 
+TEMPLATE_DEBUG = DEBUG
 PROJECT_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)))
 
 ADMINS = (
@@ -54,7 +55,7 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = ''
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -166,8 +167,6 @@ MEDIA_URL = "/media/"
 if 'test' in sys.argv:
   DATABASE_ENGINE = 'sqlite3'          
   DATABASE_NAME = 'test.db'
-
-TEMPLATE_DEBUG = DEBUG
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 FIXTURE_DIRS = (os.path.join(PROJECT_ROOT, 'fixtures'),)
