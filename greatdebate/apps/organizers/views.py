65e9b793501010c738330b8439c241a43083c1f4
Daniel Mican <dm03514@gmail.com>
2011-06-19 03:02:50 +0000

    Home Page

--- greatdebate/apps/organizers/views.py
+++ greatdebate/apps/organizers/views.py
@@ -1,13 +1,16 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.views.decorators.http import requires_POST
+from django.shortcuts import render_to_response
+from django.views.decorators.http import require_POST
 from greatdebate.apps.organizers.models import Organizer
 
 def new_organizer(request):
   required_fields_list = [
     'email',
   ]
-
   organizer_insert_dict = {'email':request.POST['email']}
   organizer = Organizer(**organizer_insert_dict)
   organizer.save()
-  return HttpResponseRedirect('/create_campaign')
+  return HttpResponseRedirect('/create_campaign/')
+
+def render_home(request):
+  return render_to_response('home.html')
